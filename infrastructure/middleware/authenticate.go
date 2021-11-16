7bc8b975776c0370b748ad4a8bd1312c0f1f4eb1
Janitra Panji <janitra.panji@redtech.co.id>
2021-11-16 22:55:03 +0700

    Add middleware auth

---
+++ infrastructure/middleware/authenticate.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	"go-boilerplate-clean-arch/config"
+	"go-boilerplate-clean-arch/utils"
+
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+)
+
+func Authenticate() func(ctx *fiber.Ctx) error {
+	return jwtware.New(jwtware.Config{
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			return utils.ApiUnauthorized(ctx, "Unauthorized user", err)
+		},
+		SigningKey: []byte(config.Config("JWT_SECRET")),
+	})
+}
--- modules/auth/route.go
+++ modules/auth/route.go
@@ -1,12 +1,11 @@
 package auth
 
 import (
-	"go-boilerplate-clean-arch/config"
+	"go-boilerplate-clean-arch/infrastructure/middleware"
 	"go-boilerplate-clean-arch/modules/auth/handlers"
 	"go-boilerplate-clean-arch/utils"
 
 	"github.com/gofiber/fiber/v2"
-	jwtware "github.com/gofiber/jwt/v3"
 )
 
 type ApiRoute struct {
@@ -20,9 +19,5 @@ func (handler *ApiRoute) Route(app fiber.Router) {
 
 	user.Post("/", handler.AuthHandler.Authentication)
 
-	user.Use(jwtware.New(jwtware.Config{
-		SigningKey: []byte(config.Config("JWT_SECRET")),
-	}))
-
-	user.Get("/me", handler.AuthHandler.GetProfile)
+	user.Get("/me", middleware.Authenticate(), handler.AuthHandler.GetProfile)
 }
