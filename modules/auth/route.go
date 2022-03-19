f6d7044f8e9149b844ad50136486273fbf60f193
Janitra Panji <bonkzero404@gmail.com>
2022-03-19 23:07:31 +0700

    Route group

--- modules/auth/route.go
+++ modules/auth/route.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"go-starterkit-project/app/middleware"
+	"go-starterkit-project/database/driver"
 	"go-starterkit-project/modules/auth/handlers"
 	"go-starterkit-project/utils"
 
@@ -12,14 +13,29 @@ type ApiRoute struct {
 	AuthHandler handlers.AuthHandler
 }
 
+func registerEndpointRole(name string) string {
+
+	role, _ := driver.Casbin()
+
+	role.AddPolicy("user", name, "", "read", "", "")
+
+	return name
+}
+
 func (handler *ApiRoute) Route(app fiber.Router) {
 	const endpointGroup string = "/auth"
 
-	user := app.Group(utils.SetupApiGroup() + endpointGroup)
+	uriGroup := utils.SetupApiGroup() + endpointGroup
+	user := app.Group(uriGroup)
 
 	user.Post("/", middleware.RateLimiter(5, 120), handler.AuthHandler.Authentication)
 
-	user.Get("/me", middleware.Authenticate(), middleware.RateLimiter(5, 30), handler.AuthHandler.GetProfile)
-
 	user.Get("/refresh-token", middleware.Authenticate(), middleware.RateLimiter(5, 30), handler.AuthHandler.RefreshToken)
+
+	user.Name(registerEndpointRole("auth.me")).Get(
+		"/me",
+		middleware.Authenticate(),
+		middleware.RateLimiter(5, 30),
+		handler.AuthHandler.GetProfile,
+	)
 }
